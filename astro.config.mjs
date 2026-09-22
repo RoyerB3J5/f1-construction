f9542942c8ae99a3fbb317d50ac6a48b0c50319b
Royer Bonifacio <inkshapead@gmail.com>
2026-09-22 02:21:27 -0500

    add:video

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
@@ -19,4 +22,6 @@ export default defineConfig({
       redirectToDefaultLocale: false,
     },
   },
+
+  adapter: vercel(),
 });
